3e03a47b842b4c5de6ef1ddacd3b6673970a5211
simonamilanova <smilanova97@gmail.com>
2021-01-28 02:25:50 +0100

    New homework from Class08

---
+++ Homework Class08/class08.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    //HOMEWORK PART 1
+    // $("button").click(function(){
+    //     var userInput = $("input").val();
+    //     $("h1").text(`Hello there ${userInput}`);
+    // })
+
+
+    //HOMEWORK PART 2
+//     $("button").click(function(){
+//         var userText = $("#text").val();
+//         var userColor = $("#color").val();
+
+//         $("button").after(`<h1>${userText}</h1>`);
+//         $("h1").css("color", `${userColor}`);
+    
+//         if(userColor == "" || userText == "" ){
+//             $("h3").text("Error");
+//         }
+   
+//    });
+
+});
