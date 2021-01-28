90346695fbe2666432bcdc16a1de8cd732387028
simonamilanova <smilanova97@gmail.com>
2021-01-28 23:25:53 +0100

    Small change

--- Homework Class06/class06.js
+++ Homework Class06/class06.js
@@ -10,3 +10,15 @@ function  createTable() {
         cell.innerHTML = "Row- " + r + " Col- " + c;
       }
 }
+
+/*difference between keypress, keydown, keyup
+
+keyup - fires when the user releases a key, after the default action of that key has been performed. 
+Keypress - fires when an actual character is being inserted in, for instance, a text input. It repeats while the user keeps the key depressed.
+Keydown fires when the user depresses a key. It repeats while the user keeps the key depressed.
+
+difference betwenn mouseover, mouseout and mouseleave 
+
+mouseleave is fired when the pointer has exited the element and all of its descendants 
+mouseout is fired when the pointer leaves the element or leaves one of the element's descendants
+mouseover is fired at an element when the mouse is used to move the cursor onto the element or one of its child elements.*/
